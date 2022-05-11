70e535063fc90562e6f05a8e4ee7af67954fd6a4
yuririnnnu <yrk96424@gmail.com>
2022-05-11 17:46:41 -0400

    sending http post request

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,44 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const prompts = [
+    'Tell us about your favorite food?',
+    'Tell us about your favorite color?',
+    'Tell us about your favorite animal?',
+    'Tell us about your favorite movie?',
+    'Tell us about your favorite snack?',
+]
 const initialInput = {
-    prompt: '',
     answer: ''
 }
-
 const Form = () => {
+    const secret = process.env.REACT_APP_OPENAI_SECRET
     const [input, setInput] = useState(initialInput)
-
+    const [prompt, setPrompt] = useState('')
     const handleChange = e => {
-        setInput({
-            prompt: e.target.prompt,
-            answer: e.target.answer
-        })
+        setInput({            
+            [e.target.name]: e.target.value
+        })        
     }
 
     const handleSubmit = e => {
         e.preventDefault()
-        axios.post('')
+        let dataToSend = {
+            prompt: prompt,
+            input: input.answer
+        }
+        console.log(dataToSend)
+        axios.post('https://api.openai.com/v1/engines/text-curie-001/completions', JSON.stringify(dataToSend), {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${secret}`
+            }
+        })
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
-
+    useEffect(() => {
+        let num = Math.floor(Math.random()*4)
+        setPrompt(prompts[num])
+    }, [])
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <label for='prompt'>Name</label>
+                <label htmlFor='prompt'>Prompt</label>
+                <h2>{prompt}</h2>
+                <label htmlFor='answer'>Your Answer</label>
                 <input 
-                value={input.prompt}
-                onChange={handleChange}                
-                placeholder='Prompt'
-                name='prompt'
-                type='text'
-                />
-                <label for='answer'></label>
-                <input 
-                value={input.answer}
-                onChange={handleChange}
-                placeholder='You can input anything here'
-                name='answer'
-                type='text'
+                    value={input.answer}
+                    onChange={handleChange}
+                    placeholder='You can input anything here'
+                    name='answer'
+                    type='text'
                 />
                 <button>Submit</button>
             </form>
